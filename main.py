0c5e6210f5cc376db5a4179d21182a11420adda0
Abhay08082000 <bishtabhay8@gmail.com>
2024-12-25 13:32:24 +0530

    Data Ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.Loan_defaulter import logger
+from src.Loan_defaulter.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME="Data Ingestion stage"
+try:
+    logger.info(f'>>>>>>stage {STAGE_NAME} started <<<<<<<')
+    obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>stage {STAGE_NAME} completed <<<<<<<')
+except Exception as e:
+    logger.exception(e)
+    raise e
